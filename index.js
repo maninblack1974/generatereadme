0456a826b270b42942ca1af8e76e4fe78408ecef
DRH <25594179+maninblack1974@users.noreply.github.com>
2020-12-06 22:25:25 -0800

    Adding newly created files to the project

---
+++ index.js
@@ -0,0 +1,141 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+//Create a function to prompt the user with the questions needed in order to complete and the create the READEME file
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is your Project Title?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Enter a brief description of what your project will accomplish?',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Provide an installation guide in order for a user to run this project?',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'How would you like your your project to be used?',
+    },
+    {
+        type: 'input',
+        name: 'contributors',
+        message: 'Enter your GitHub Username',
+      },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username',
+    },
+    {
+      type: 'input',
+      name: 'linkedin',
+      message: 'Enter your LinkedIn URL.',
+    },
+  ]);
+};
+
+const generateReadMe = (answers) =>
+`# Your Project Title
+
+## Description 
+
+Your GitHub profile is an extremely important aspect of your public identity as a developer. A well-crafted one allows you to show off your work to other developers as well as potential employers. An important component of your GitHub profile—and one that many new developers often overlook—is the README.md file.
+
+The quality of a README often differentiates a good project from a bad project. A good one takes advantage of the opportunity to explain and showcase what your application does, justify the technologies used, and even talk about some of the challenges you faced and features you hope to implement in the future. A good README helps you stand out among the large crowd of developers putting their work on GitHub.
+
+There's no one right way to structure a good README. There is one very wrong way, however, and that is to not include a README at all or to create a very anemic one. This guide outlines a few best practices. As you progress in your career, you will develop your own ideas about what makes a good README.
+
+At a minimum, your project README needs a title and a short description explaining the what, why, and how. What was your motivation? Why did you build this project? (Note: The answer is not "Because it was a homework assignment.") What problem does it solve? What did you learn? What makes your project stand out? If your project has a lot of features, consider adding a heading called "Features" and listing them here.
+
+If you're new to Markdown, read the GitHub guide on [Mastering Markdown](https://guides.github.com/features/mastering-markdown/).
+
+If you need an example of a good README, check out [the VSCode repository](https://github.com/microsoft/vscode).
+
+
+## Table of Contents (Optional)
+
+If your README is very long, add a table of contents to make it easy for users to find what they need.
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+
+
+## Installation
+
+What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+
+
+## Usage 
+
+Provide instructions and examples for use. Include screenshots as needed. 
+
+
+## Credits
+
+List your collaborators, if any, with links to their GitHub profiles.
+
+If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
+
+If you followed tutorials, include links to those here as well.
+
+
+
+## License
+
+The last section of a good README is a license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, use [https://choosealicense.com/](https://choosealicense.com/)
+
+
+---
+
+🏆 The sections listed above are the minimum for a good README, but your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
+
+## Badges
+
+![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
+
+Badges aren't _necessary_, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+
+
+## Contributing
+
+If you created an application or package and would like other developers to contribute it, you will want to add guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own.
+
+## Tests
+
+Go the extra mile and write tests for your application. Then provide examples on how to run them.
+
+
+---
+© 2019 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.
+`;
+
+// Bonus using async/await and try/catch
+const init = async () => {
+  console.log('hi');
+  try {
+    const answers = await promptUser();
+
+    const readMe = generateReadMe(answers);
+
+    await writeFileAsync('README.md', readMe);
+
+    console.log('Successfully wrote to index.html');
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+init();
